38986e34821128eb082bb61a7738db1f5f1fbb81
Rudy <rudy.prihantoro@xtremax.com>
2026-02-17 21:46:56 +0700

    feat(api): add Permissions (Me) API route for user permissions retrieval
    feat(services): update index to include new permissions types and services
    
    chore(registry): enhance registry.json with new permissions API route and update dependencies formatting

--- packages/cli/templates/lib/services/index.ts
+++ packages/cli/templates/lib/services/index.ts
@@ -1,26 +1,32 @@
 /**
  * Microbuild Services
- * 
+ *
  * Re-exports all service classes.
  * This file is copied to your project and can be customized.
  */
 
-export { ItemsService, createItemsService } from './items';
-export { FieldsService, createFieldsService } from './fields';
-export { CollectionsService, createCollectionsService } from './collections';
-export { PermissionsService, createPermissionsService, type FieldPermissions } from './permissions';
-export { apiRequest, type ApiRequestOptions } from './api-request';
+export { apiRequest, type ApiRequestOptions } from "./api-request";
+export { CollectionsService, createCollectionsService } from "./collections";
+export { FieldsService, createFieldsService } from "./fields";
+export { ItemsService, createItemsService } from "./items";
+export {
+  PermissionsService,
+  createPermissionsService,
+  type CollectionAccess,
+  type CollectionActionAccess,
+  type FieldPermissions,
+} from "./permissions";
 
 // DaaS Context Provider for direct API access (bypassing proxy routes)
 export {
   DaaSProvider,
-  useDaaSContext,
-  useIsDirectDaaSMode,
-  setGlobalDaaSConfig,
-  getGlobalDaaSConfig,
   buildApiUrl,
   getApiHeaders,
+  getGlobalDaaSConfig,
+  setGlobalDaaSConfig,
+  useDaaSContext,
+  useIsDirectDaaSMode,
   type DaaSConfig,
   type DaaSContextValue,
   type DaaSProviderProps,
-} from './daas-context';
+} from "./daas-context";
